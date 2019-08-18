ff2a18424f42a31c8b8c365f16154626b978dd6f
Jack <jack.bonser@ynap.com>
2019-08-18 13:18:03 +0100

    added hack to use EU flag instead of country flag

--- app/src/main/java/com/example/currencyapp/api/InternalCountryMapping.kt
+++ app/src/main/java/com/example/currencyapp/api/InternalCountryMapping.kt
@@ -5,13 +5,21 @@ import com.example.currencyapp.api.response.InternalCountry
 
 object InternalCountryMapping {
 
+    private const val EUR = "EUR"
+
     fun mapCountries(response: List<InternalCountry>?) = response?.distinctBy {
         it.currencies?.firstOrNull()?.code
     }?.map {
         Country(
             it.currencies?.firstOrNull()?.code,
             it.currencies?.firstOrNull()?.name,
-            it.flag
+            if (it.currencies?.firstOrNull()?.code == EUR) {
+                // bad hack to use EU flag instead of country flag
+                EUR
+            } else {
+                it.flag
+            }
         )
     }
+
 }
