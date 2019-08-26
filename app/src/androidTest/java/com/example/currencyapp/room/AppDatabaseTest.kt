f817ccc9ec69467f107dd8255175e075df9c08f8
Jack <jack.bonser@ynap.com>
2019-08-26 19:42:38 +0100

    added database test

--- app/src/androidTest/java/com/example/currencyapp/room/AppDatabaseTest.kt
+++ app/src/androidTest/java/com/example/currencyapp/room/AppDatabaseTest.kt
@@ -43,7 +43,39 @@ class AppDatabaseTest {
         assert(dbResult.size == 1)
         assertEquals("EUR", dbResult[0].currencyCode)
         assertEquals("euro", dbResult[0].currencyDescription)
-        assertEquals("EUR", dbResult[0].currencyFlag)
+        assertEquals("flag", dbResult[0].currencyFlag)
         assertEquals(1.0F, dbResult[0].currencyRate)
+    }
+
+
+    @Test
+    fun updateCurrencyData() {
+        val currencyRates = listOf(
+            CurrencyRate(
+                currencyCode = "EUR",
+                currencyDescription = "euro",
+                currencyFlag = "flag",
+                currencyRate = 1.0F
+            )
+        )
+
+        appDatabase.currencyDao().insertAll(currencyRates)
+
+        val updatedRates = listOf(
+            CurrencyRate(
+                currencyCode = "EUR",
+                currencyDescription = "euro",
+                currencyFlag = "flag",
+                currencyRate = 1.5F
+            )
+        )
+
+        appDatabase.currencyDao().updateCurrencies(updatedRates)
+        val dbResult = appDatabase.currencyDao().getAll()
+        assert(dbResult.size == 1)
+        assertEquals("EUR", dbResult[0].currencyCode)
+        assertEquals("euro", dbResult[0].currencyDescription)
+        assertEquals("flag", dbResult[0].currencyFlag)
+        assertEquals(1.5F, dbResult[0].currencyRate)
     }
 }
